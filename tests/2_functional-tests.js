a264bf0c0dd53dff09354123dcd22d40c0f5f8fc
Rifat Hossain <rifat.hossain.dhrubo@gmail.com>
2020-03-21 18:25:02 +0600

    tests added

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -10,28 +10,152 @@ const chaiHttp = require('chai-http');
 const chai = require('chai');
 
 const { assert } = chai;
-const server = require('../server');
+const server = require('./server');
 
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
   suite('API ROUTING FOR /api/threads/:board', function() {
-    suite('POST', function() {});
+    suite('POST', function() {
+      test('POST thread', function(done) {
+        chai
+          .request(server)
+          .post('/api/threads/general')
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.isObject(res.body, 'response should be an object');
+            assert.property(res.body._id, 'id', 'body should have a _id');
+            assert.property(
+              res.body.delete_password,
+              'false',
+              ' should have a default value of false'
+            );
+          });
+        done();
+      });
+    });
 
-    suite('GET', function() {});
+    suite('GET', function() {
+      test('GET thread', function(done) {
+        chai
+          .request(server)
+          .post('/api/threads/general')
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.isObject(res.body, 'response should be an object');
+            assert.property(
+              res.body.data,
+              'data',
+              'body should have a data property'
+            );
+            assert.isArray(
+              res.body.data,
+              'data array',
+              'data should be an array'
+            );
+          });
+        done();
+      });
+    });
 
-    suite('DELETE', function() {});
+    suite('DELETE', function() {
+      test('GET thread', function(done) {
+        chai
+          .request(server)
+          .post('/api/threads/general')
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.isString(res.body, 'response should be a string');
+            assert.equal(re.body, 'success');
+          });
+        done();
+      });
+    });
 
-    suite('PUT', function() {});
+    suite('PUT', function() {
+      test('GET thread', function(done) {
+        chai
+          .request(server)
+          .post('/api/threads/general')
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.isString(res.body, 'response should be a string');
+            assert.equal(re.body, 'success');
+          });
+        done();
+      });
+    });
   });
 
   suite('API ROUTING FOR /api/replies/:board', function() {
-    suite('POST', function() {});
+    suite('POST', function() {
+      test('POST thread', function(done) {
+        chai
+          .request(server)
+          .post('/api/threads/general')
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.isObject(res.body, 'response should be an object');
+            assert.property(res.body._id, 'id', 'body should have a _id');
+            assert.property(
+              res.body.delete_password,
+              'false',
+              ' should have a default value of false'
+            );
+          });
+        done();
+      });
+    });
 
-    suite('GET', function() {});
+    suite('GET', function() {
+      test('GET thread', function(done) {
+        chai
+          .request(server)
+          .post('/api/threads/general')
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.isObject(res.body, 'response should be an object');
+            assert.property(
+              res.body.data,
+              'data',
+              'body should have a data property'
+            );
+            assert.isArray(
+              res.body.data,
+              'data array',
+              'data should be an array'
+            );
+          });
+        done();
+      });
+    });
 
-    suite('PUT', function() {});
+    suite('DELETE', function() {
+      test('GET thread', function(done) {
+        chai
+          .request(server)
+          .post('/api/threads/general')
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.isString(res.body, 'response should be a string');
+            assert.equal(res.body, 'success');
+          });
+        done();
+      });
+    });
 
-    suite('DELETE', function() {});
+    suite('PUT', function() {
+      test('GET thread', function(done) {
+        chai
+          .request(server)
+          .post('/api/threads/general')
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.isString(res.body, 'response should be a string');
+            assert.equal(res.body, 'success');
+          });
+        done();
+      });
+    });
   });
 });
